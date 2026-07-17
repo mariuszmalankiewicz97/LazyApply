d39073dacbbecc2e30787e96cbb846cc182c2862
Mariusz <mariuszmalankiewicz@gmail.com>
2026-07-17 15:39:36 +0200

    feat: update static HTML injection via Jinja2 add HTMLInjection class and main.py for core logic

---
+++ generators/html_injector.py
@@ -0,0 +1,18 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+class HTMLInjector:
+    def __init__(self, directory_templates="templates"):
+        self.directory_templates = directory_templates
+
+        loader_templates = FileSystemLoader(self.directory_templates)
+
+        self.environment = Environment(loader=loader_templates)
+
+    def inject_data(self, fetch_template):
+        template = self.environment.get_template(fetch_template)
+
+        html = template.render(name="MARIUSZ")
+
+        with open("generate.html", "w", encoding="utf-8") as f:
+            f.write(html)
---
+++ main.py
@@ -0,0 +1,4 @@
+from generators.html_injector import HTMLInjector
+
+injector_html = HTMLInjector()
+injector_html.inject_data("test.html")
